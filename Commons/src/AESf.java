e7a66b275b5cbd836418146b8a9e0bcec7731026
limofan41 <963400451@qq.com>
2017-12-05 21:20:22 -0600

    masterkey generation and send it to server

--- Commons/src/AESf.java
+++ Commons/src/AESf.java
@@ -251,6 +251,17 @@ public class AESf implements Printable {
         return sha1Encode;
 
     }
+	
+	
+	public static String getRandomString(int length) {
+	    String KeyString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	    StringBuffer sb = new StringBuffer();
+	    int len = KeyString.length();
+	    for (int i = 0; i < length; i++) {
+	       sb.append(KeyString.charAt((int) Math.round(Math.random() * (len - 1))));
+	    }
+	    return sb.toString();
+	}
 
 
 }
