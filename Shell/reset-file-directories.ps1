443d629fe1553702d257cca6bfd3f7cbea107184
baonguyen96 <baonguyendrump@gmail.com>
2019-04-20 19:11:58 -0500

    Update reset-file-directories.ps1

--- Shell/reset-file-directories.ps1
+++ Shell/reset-file-directories.ps1
@@ -1,3 +1,12 @@
-foreach ($directory in 'Server', 'Client') {
-    Remove-Item "..\$directory\src\main\resources\FilesDirectory\*.*"
+foreach ($directory in 'Server', 'Client')
+{
+    $path = "..\$directory\src\main\resources\FilesDirectory\"
+    if(Test-Path $path)
+    {
+        Get-Childitem "..\$directory\src\main\resources\FilesDirectory\" -File | Foreach-Object {Remove-Item $_.FullName}
+    }
+    else
+    {
+        mkdir $path
+    }
 }
